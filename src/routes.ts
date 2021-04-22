796ec109538cb4b69a93be142b209a70176f3eb3
Tassio Gama <tassiogbarbosa@gmail.com>
2021-04-21 21:22:25 -0300

    confirmar

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-
-import { SettingsRepository } from "./repositories/SettingRepository";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-routes.post("/settings", async (request, response) => {
-    const { chat , username } = request.body
-    const settingReposytory = getCustomRepository(SettingsRepository);
+const settingsController = new SettingsController();
 
-    const settings = settingReposytory.create({
-        chat,
-        username,
-    });
-
-    await settingReposytory.save(settings);
-
-    return response.json(settings);
-});
+routes.post("/settings", settingsController.create);
 
 export { routes };
