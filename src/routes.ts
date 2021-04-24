ad03517c48133008e20e04db2dc1d4167cf41b20
Tassio Gama <tassiogbarbosa@gmail.com>
2021-04-23 22:38:03 -0300

    confirmar

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
 
@@ -6,8 +7,13 @@ const routes = Router();
 
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messageController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
+
 routes.post("/users", usersController.create);
+
+routes.post("/messages", messageController.create);
+routes.get("/messages/:id", messageController.showByUser);
 
 export { routes };
--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,32 +1,38 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingRepository";
 
-interface ISettingsCreate{
+interface ISettingsCreate {
     chat: boolean;
     username: string;
 }
 
 class SettingsService {
-    async create({ chat, username } : ISettingsCreate) {
-        const settingsRepository = getCustomRepository(SettingsRepository);
+    private settingsRepository: Repository<Setting>;
+
+    constructor() {
+        this.settingsRepository = getCustomRepository(SettingsRepository);
+    }
+
+    async create({ chat, username }: ISettingsCreate) {
 
         //Select * from settings where username = "username" limit 1;
-        const userAlreadyExist = await settingsRepository.findOne({
+        const userAlreadyExist = await this.settingsRepository.findOne({
             username
-        }) ;
+        });
 
-        if(userAlreadyExist) {
+        if (userAlreadyExist) {
             throw new Error("User Already Exist!");
         }
-    
-        const settings = settingsRepository.create({
+
+        const settings = this.settingsRepository.create({
             chat,
             username,
         });
-    
-    await settingsRepository.save(settings)
 
-    return settings;
+        await this.settingsRepository.save(settings)
+
+        return settings;
     }
 }
 
