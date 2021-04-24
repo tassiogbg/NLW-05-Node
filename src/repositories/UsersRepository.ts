2293b7e96c08d34f1b65383c31f50006fac81eaf
Tassio Gama <tassiogbarbosa@gmail.com>
2021-04-24 20:50:45 -0300

    confirmar

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -1,6 +1,6 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "../entities/User";
+import { EntityRepository, Repository } from 'typeorm';
 
+import { User } from '../entities/User';
 
 @EntityRepository(User)
 class UsersRepository extends Repository<User> { }
